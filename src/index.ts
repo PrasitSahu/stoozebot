cbb91c4c7a17500d0b20b42b7c4778de4bbe2fd4
Prasit Sahu <prasitsahu4@gmail.com>
2026-03-11 18:52:11 +0530

    feat: add check for bot secret

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
+import { drizzle } from "drizzle-orm/d1";
 import { Bot, webhookCallback } from "grammy";
 import { BotContext } from "./config";
-import developer from "./middlewares/developer";
-import { registerCommands } from "./handlers/register";
-import { drizzle } from "drizzle-orm/d1";
 import * as schema from "./db/index";
+import { registerCommands } from "./handlers/register";
 import { auth } from "./middlewares/auth";
+import developer from "./middlewares/developer";
+
+function verifyBot(secret: string) {
+	return secret === process.env.BOT_SECRET;
+}
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
+		if (!verifyBot(request.headers.get("X-Telegram-Bot-Api-Secret-Token") || "")) {
+			return new Response("Bad Request", { status: 400 });
+		}
+
 		const bot = new Bot<BotContext>(env.BOT_TOKEN);
 		const db = drizzle(env.DB, {
 			schema,
